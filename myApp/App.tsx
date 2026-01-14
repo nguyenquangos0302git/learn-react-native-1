42998fcb9fb430ccaf1d2bbc17a6adaf41be4166
nguyenquangos0302git <nguyenquangos0302git@gmail.com>
2026-01-14 17:31:47 +0700

    video 30

--- myApp/App.tsx
+++ myApp/App.tsx
@@ -1,16 +1,29 @@
-import { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
-  const [name, setName] = useState<string>("abc");
+  const [name, setName] = useState<string>();
   const [age, setAge] = useState<number>(0);
 
   return (
     <View style={styles.container}>
       <View>
         <Text style={styles.text}>{name}</Text>
+        <TextInput
+          onChangeText={(v) => setName(v)}
+          value={name}
+          autoCapitalize="none"
+          autoCorrect={false}
+          style={{
+            borderColor: "black",
+            borderWidth: 1,
+          }}
+        />
+        <Button title="add new" />
+        <Text style={styles.text}>{name}</Text>
       </View>
-      <Text>Test</Text>
+      <Text>Test2</Text>
+      <Text>Test2</Text>
     </View>
   );
 }
